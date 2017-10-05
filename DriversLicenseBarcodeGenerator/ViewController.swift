2478cc1e85f0878613e1cb3a7baf55b30918ca38
Kyle Decot <kyle@joinroot.com>
2017-10-05 10:32:03 -0400

    Initial Commit

---
+++ DriversLicenseBarcodeGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DriversLicenseBarcodeGenerator
+//
+//  Created by Kyle Decot on 10/5/17.
+//  Copyright © 2017 Kyle Decot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
